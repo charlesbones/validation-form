724e9e9d9ed90c7886cc7c6603746d1f6d0cf3bb
Carlos Rodriguez <c.rodriguez@arduino.cc>
2017-08-24 11:32:51 +0200

    add php contact

--- contact.php
+++ contact.php
@@ -16,7 +16,43 @@
         array_push($files,$server_file);
       }
     }
-    $recipient="ctc.101@arduino.cc";
+    $to = "c.rodriguez@bcmi-labs.cc"
+    $from = $_POST["sender1"];
+    $subject = "CTC validation form $date[month] $date[mday] $date[year]";
+    $message = "hola";
+    $headers = "From: $from";
+
+    $semi_rand = md5(time());
+    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"
+
+    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
+
+  	// multipart boundary
+  	$message = "This is a multi-part message in MIME format.\n\n" . "--{$mime_boundary}\n" . "Content-Type: text/plain; charset=\"iso-8859-1\"\n" . "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n";
+  	$message .= "--{$mime_boundary}\n";
+
+  	// preparing attachments
+  	for($x=0;$x<count($files);$x++){
+  		$file = fopen($files[$x]['tmp_name'],"rb");
+  		$data = fread($file,filesize($files[$x]['tmp_name']));
+  		fclose($file);
+  		$data = chunk_split(base64_encode($data));
+  		$name = $files[$x]['name'];
+  		$message .= "Content-Type: {\"application/octet-stream\"};\n" . " name=\"$name\"\n" .
+  		"Content-Disposition: attachment;\n" . " filename=\"$name\"\n" .
+  		"Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+  		$message .= "--{$mime_boundary}\n";
+  	}
+  	// send
+
+  	$ok = mail($to, $subject, $message, $headers);
+  	if ($ok) {
+  		echo "<p>mail sent to $to!</p>";
+  	} else {
+  		echo "<p>mail could not be sent!</p>";
+  	} 
+
+    /*$recipient="ctc.101@arduino.cc";
     $date=getdate(date("U"));
     $subject="CTC validation form $date[month] $date[mday] $date[year]";
     $sender=$_POST["name1"];
@@ -30,6 +66,6 @@
 
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
+    $thankYou="<p>Thank you! Your message has been sent.</p>";*/
   }
  ?>
